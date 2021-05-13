60b70a25e10d8fbfd0d7f3c2f37459cac750f455
baalajimaestro <me@baalajimaestro.me>
2021-05-13 12:38:52 +0000

    rebrand

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Weeb Kernel by idkwhoiam322
+kernel.string=Retarded Kernel by baalajimaestro
 do.devicecheck=1
 do.modules=0
 do.systemless=1
